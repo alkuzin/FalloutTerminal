385edee34ef67cd3ff0a888b9e241b5d4dc8019b
alkuzin <->
2023-10-06 23:24:59 +0300

    Updated main.c

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 int main(void)
 {
     const int size = 3;
-    option_t* option_list[size];
+    option_t option_list[size];
 
-    option_t option_1 = {.title="option 1"};
-    option_t option_2 = {.title="option 2"};
-    option_t option_3 = {.title="option 3"};
+    option_list[0] = (option_t){.title="option 1", .content="-"};
+    option_list[1] = (option_t){.title="option 2"};
+    option_list[2] = (option_t){.title="option 3"};
 
     print_options(option_list, size);
     
