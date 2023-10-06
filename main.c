e5a0f60ccdf5a5927c44c964f4a7296727a2dec2
alkuzin <->
2023-10-07 00:37:00 +0300

    Updated main.c

--- main.c
+++ main.c
@@ -4,16 +4,27 @@
 
 int main(void)
 {
+    // char ch;
     const int size = 3;
     option_t option_list[size];
 
-    option_list[0] = (option_t){.title="option 1", .content="-"};
-    option_list[1] = (option_t){.title="option 2"};
-    option_list[2] = (option_t){.title="option 3"};
+    option_list[0] = (option_t){.title="View Access Logs", .content=access_logs_text, .content_type="text"};
+    option_list[1] = (option_t){.title="Incident Reports", .content="-", .content_type="text"};
+    option_list[2] = (option_t){.title="Personal Notes", .content="-", .content_type="directory"};
 
+    set_main_title("==== Institute Central Network ====\n"
+                   "Advanced Systems Terminal 1A\n\n");
+
+    // ch = getch();
+
+    slow_print(main_title, DEFAULT_DELAY);
     print_options(option_list, size);
-    
-    
+
+    getchar();
+    getchar();
+    system("clear");
+    slow_print(main_title, DEFAULT_DELAY);
+    slow_print(option_list[0].content, DEFAULT_DELAY);
 
     return 0;
 }
