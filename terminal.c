37289f1705736713e9079bd0154dfd1387e31a8a
alkuzin <->
2023-10-06 23:24:43 +0300

    Updated terminal.c

--- terminal.c
+++ terminal.c
@@ -1,7 +1,11 @@
 #include "terminal.h"
 
+char main_title[128];
+
 void print_options(option_t* options_list, int size) 
 {
     for (int i = 0; i < size; i++)
-        printf("[%-s]", options_list[i].title);
+        printf("[%-s]\n", options_list[i].title);
 }
+
+void set_main_title(char* title);
