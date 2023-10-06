a249630490837e5868502c688d059ae7c9bcb41b
alkuzin <->
2023-10-07 00:37:32 +0300

    Updated terminal.h

--- terminal.h
+++ terminal.h
@@ -2,12 +2,18 @@
 #define TERMINAL_H
 
 #include <stdio.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
 
 extern char main_title[128];
+extern char access_logs_text[512];
+#define DEFAULT_DELAY 50
 
 typedef struct
 {
     char title[32];
+    char content_type[16];
     void* content;
 }option_t;
 
@@ -15,5 +21,11 @@ typedef struct
 void print_options(option_t* options_list, int size);
 
 void set_main_title(char* title);
+
+void set_option_content(option_t* option, void* content);
+
+void slow_print(char* text, int delay);
+
+int getch(void);
 
 #endif
