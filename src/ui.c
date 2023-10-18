6a1db4dedcaa750fa841a0e0514e9e3d3fb646c4
alkuzin <->
2023-10-18 23:22:20 +0300

    Update ui.c

--- src/ui.c
+++ src/ui.c
@@ -1,2 +1,29 @@
 #include "../include/ui.h"
 
+char primary_color[COLOR_SIZE];
+char error_color[COLOR_SIZE];
+
+void set_primary_color(const char* color)
+{
+    strncpy(primary_color, color, COLOR_SIZE);
+}
+
+void set_error_color(const char* color)
+{
+    strncpy(error_color, color, COLOR_SIZE);
+}
+
+void puts_col(const char* string)
+{
+    printf("%s%s%s\n", primary_color, string, COLOR_RESET);
+}
+
+void puts_rev_col(const char* string)
+{
+    printf("%s%s%s%s\n", COLOR_REVERSE, primary_color, string, COLOR_RESET);
+}
+
+void puts_err_col(const char* string)
+{
+    printf("%s%s%s\n", error_color, string, COLOR_RESET);
+}
