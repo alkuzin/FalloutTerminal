2df317f7c906f21afd3cefced812cdb7a99a3312
alkuzin <->
2023-10-18 23:21:09 +0300

    Update ui.h

--- include/ui.h
+++ include/ui.h
@@ -2,14 +2,34 @@
 #define UI_H
 
 #include <stdio.h>
+#include <string.h>
 
-extern char* main_color;
-#define RED "\033[31m"
-#define BLACK "\033[30m"
-#define GREEN "\033[32m"
-#define CYAN "\033[96m"
-#define WHITE "\033[97m"
-#define REVERSE "\033[7m"
-#define RESET "\033[0m"
+#define COLOR_SIZE 6
+
+#define COLOR_WHITE     "\033[97m"
+#define COLOR_BLACK     "\033[30m"
+#define COLOR_RED       "\033[31m"
+#define COLOR_GREEN     "\033[92m"
+#define COLOR_CYAN      "\033[96m"
+  
+#define COLOR_REVERSE   "\033[7m"
+#define COLOR_RESET     "\033[0m"
+
+extern char primary_color[COLOR_SIZE];
+extern char error_color[COLOR_SIZE];
+
+void set_primary_color(const char* color);
+
+// set error messages color
+void set_error_color(const char* color);
+
+// print colored text
+void puts_col(const char* string);
+
+// print reverse colored text
+void puts_rev_col(const char* string);
+
+// print colored error text
+void puts_err_col(const char* string);
 
 #endif
