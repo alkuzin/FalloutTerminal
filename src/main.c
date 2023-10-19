7571543de1a0fa7e9d90720220086acc9ae6c2b8
alkuzin <->
2023-10-19 20:13:06 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,32 +2,14 @@
 //
 //    src/main.c	
 //
-//    By: alkuzin <---@gmail.com>                                         	
+//    By: alkuzin < >                                                     	
 //
-//    Updated: 17/10/2023 23:06:24                                             	
+//    Updated: 19/10/2023 19:54:30                                             	
 //
 //                       Copyright "FalloutTerminal" (c), 2023.	
 //
 // =============================================================================
 #include "../include/terminal.h"
-
-static char access_logs_text[CONTENT_SIZE] = 
-"Access: Local. Login: Li.M\n"
-"Notes: Review of Phase Three project status\n\n"
-"Access: Local. Login: Li.M\n"
-"Notes: update diagnostic tools for child synth project\n\n"
-"Access: Remote. Login: DIRECTOR\n"
-"Notes: --REDACTED--\n\n"
-"Access: Local. Login: Li.M\n"
-"Notes: Review of all BioScience projects\n\n\n";
-
-static char data_corrupted[512] = 
-"--ERROR DATA CORRUPTED--\n\n"
-"r9cjditxchfxirksfjiOJ)(*&^IOFBUi_)**()&...\n\n"
-"0000200f  7325 735b 2573 5d25 3634 7309 001b 5b30\n"
-"0000201f  6d00 2573 5b25 735d 2573 0025 7325 7325\n" 
-"0000202f  730a 0074 6578 7400 6469 7265 6374 6f72\n"
-"0000203f  7900 2573 0063 6c65 6172 0025 7325 7325\n";
 
 int main(void)
 {
@@ -36,23 +18,83 @@ int main(void)
     "==== Institute Central Network ====\n"
     "Advanced Systems Terminal 1A     \n\n";
 
-    set_primary_color(COLOR_CYAN);
+    set_primary_color(COLOR_WHITE);
     set_error_color(COLOR_RED);
     set_title(main_title);
 
     // set options
     int size = 3;
     option_t option_list[size];
+    option_t opt_list_personal_notes[2];
 
-    option_list[0] = (option_t){.title="View Access Logs", .content=access_logs_text, .content_type=TEXT};
-    option_list[1] = (option_t){.title="Incident Reports", .content="---> Incident Reports text <---", .content_type=TEXT};
-    option_list[2] = (option_t){.title="Personal Notes", .content=data_corrupted, .content_type=ERROR};
-    
+    opt_list_personal_notes[0] = (option_t)
+    {
+        .title="Cold Fusion",
+        .content=
+        "Proposal:\n\n"
+        "Divert time/materials from current Phase Three research to exploring the\n"
+        "possibility of sustainable nuclear reactions through electrochemical processes at\n"
+        "or near room temperatures. Pre-war work on the subject yielded no concrete\n"
+        "results; advances in technology could potentially make it possible.\n\n"
+        "Approval:\n\n"
+        "Rejected - Li.M\n"
+        "Notes: Evidence suggests this is, and always will be, a pipe dream.\n",
+        .content_type=TEXT
+    };
+
+    opt_list_personal_notes[1] = (option_t)
+    {
+        .title="Surface Work Crew Tracking",
+        .content=
+        "Group Ident: G1486\n"
+        "Assigned Units: R7-81, T3-54, B8-88, J1-74\n"
+        "Location: Site Alpha\n"
+        "Elapsed Time: 160:33:12\n"
+        "Last Report: 1:56:40\n\n"
+        "NOTES:\n"
+        "Unit B8-88 removed, unit T3-54 added. [ERROR, NO DATA PRESENT]\n",
+        .content_type=TEXT
+    };
+
+    option_list[0] = (option_t)
+    {
+        .title="View Access Logs",
+        .content= 
+        "Access: Local. Login: Li.M\n"
+        "Notes: Review of Phase Three project status\n\n"
+        "Access: Local. Login: Li.M\n"
+        "Notes: update diagnostic tools for child synth project\n\n"
+        "Access: Remote. Login: DIRECTOR\n"
+        "Notes: --REDACTED--\n\n"
+        "Access: Local. Login: Li.M\n"
+        "Notes: Review of all BioScience projects\n\n\n",
+        .content_type=TEXT
+    };
+
+    option_list[1] = (option_t)
+    {
+        .title="Incident Reports",
+        .content=
+        "   ****Incident Reports****\n\n\n"
+        "Incident IN2845t\n\n"
+        "Patient: Thompson.E\n"
+        "Presenting symptom: First-degree burns, left hand\n"
+        "Notes: Patient placed his hand inside power\n"
+        "relay without first disengaging. First aid applied.\n",     
+        .content_type=TEXT
+    };
+
+    option_list[2] = (option_t)
+    {
+        .title="Personal Notes", 
+        .content=&opt_list_personal_notes,
+        .content_type=DIRECTORY
+    };
+
     // initializing terminal
     hide_cursor();
-    puts_col("... Print any button to start ...");
+    puts_col("..Initializing terminal..\nPress any key to continue.\n\n");
     slow_print(title, DEFAULT_DELAY);
     select_option(option_list, size);
-
     return 0;
 }
