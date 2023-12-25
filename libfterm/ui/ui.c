e9027d8bb002e18f6e45c4be1f41b58f19027978
alkuzin <alkuzindev@gmail.com>
2023-12-25 22:06:41 +0300

    Updated libfterm/ui

---
+++ libfterm/ui/ui.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   ui.c                                                                     */
+/*                                                                            */
+/*   By: alkuzin <->                                                          */
+/*                                                                            */
+/*   Created: 2023/12/25 21:22:31 by alkuzin                                  */
+/*   Updated: 2023/12/25 21:35:28 by alkuzin                                  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ui.h"
+
+int primary_color;
+int error_color;
+
+void set_primary_color(int color)
+{
+    primary_color = color;
+}
+
+void set_error_color(int color)
+{
+    error_color = color;
+}
+
+void print_custom_col(const char *string, int color, int style)
+{
+    printf("\033[%dm\033[%dm%s\033[0m\n", color, style, string);
+}
+
+void print_col(const char *string)
+{
+    print_custom_col(string, primary_color, STYLE_RESET);
+}
+
+void print_rev_col(const char *string)
+{
+    print_custom_col(string, primary_color, STYLE_REVERSE);
+}
+
+void print_err_col(const char *string)
+{
+    print_custom_col(string, primary_color, error_color);
+}
+
+void print_blink_col(const char *string)
+{
+    print_custom_col(string, primary_color, COLOR_BLINK);   
+}
---
+++ libfterm/ui/ui.h
@@ -0,0 +1,53 @@
+/* *************************************************************************** */
+/*                                                                             */
+/*                                                                             */
+/*   ui.h                                                                     */
+/*                                                                             */
+/*    By: alkuzin <->                                                          */
+/*                                                                             */
+/*    Created: 2023/12/25 20:30:57 by alkuzin                                  */
+/*   Updated: 2023/12/25 21:36:00 by alkuzin                                   */
+/*                                                                             */
+/*  ************************************************************************** */
+
+#ifndef UI_H
+#define UI_H
+
+#include "../libfterm.h"
+
+enum {
+    COLOR_WHITE    = 97,
+    COLOR_BLACK    = 30,
+    COLOR_RED      = 31,
+    COLOR_GREEN    = 92,
+    COLOR_CYAN     = 96,
+
+    STYLE_BLINK    = 5,
+    STYLE_REVERSE  = 7,
+    STYLE_RESET    = 0
+};
+
+extern int primary_color;
+extern int error_color;
+
+extern void set_primary_color(int color);
+
+// set error messages color
+extern void set_error_color(int color);
+
+// print custom colored text
+extern void print_custom_col(const char *string, int color, int style);
+
+// print prime colored text
+extern void print_col(const char *string);
+
+// print reverse colored text
+extern void print_rev_col(const char *string);
+
+// print colored error text
+extern void print_err_col(const char *string);
+
+// print colored blinking text
+extern void print_blink_col(const char *string);
+
+#endif /* UI_H */
