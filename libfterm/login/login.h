d9286565be70942b5e2e8f3f25c11fac4f9ec5de
alkuzin <alkuzindev@gmail.com>
2023-12-28 13:58:21 +0300

    Updated Makefile

--- libfterm/login/login.h
+++ libfterm/login/login.h
@@ -6,13 +6,13 @@
 /*   By: alkuzin <->                                                          */
 /*                                                                            */
 /*   Created: 2023/12/25 21:53:14 by alkuzin                                  */
-/*   Updated: 2023/12/26 09:39:32 by alkuzin                                  */
+/*   Updated: 2023/12/28 12:50:01 by alkuzin                                  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LOGIN_H
 #define LOGIN_H
-
+ 
 #include "../libfterm.h"
 
 #define MAX_ATTEMPTS 5
@@ -21,27 +21,24 @@
 // This password is using for tests
 // Better to use enviroment variables in this case
 #define PASSWORD "12345"
-
 #define PASSWORD_SIZE 5
-#define PASSWORDS_COUNT 40
-#define SELECTED_PASSWORDS_COUNT 8
 
-#define SYMBOLS "!@#$?/^&*()_+=|,.{}[]-"
-#define STRING_SIZE 12
-#define STRING_ROWS 16
+#define STRING_ROWS 12
+#define STRING_SIZE 16
 
 // initialize user authorisation
 extern int login(void);
 
-// get string input from user
-extern void getinput(char* buffer);
+// successful user authorisation window
+extern void login_success(const char *success_msg);
 
-extern void lock_out_terminal(void);
+// failed user authorisation window
+extern void login_failure(const char *failure_msg);
 
+// set authorisation window title
 extern void set_login_title(char* new_title);
 
+// get authorisation window title
 extern char *get_login_title(void);
 
-extern int randint(int lower_range, int upper_range);
-
-#endif /* LOGIN_H */
+#endif /* LOGIN_H */ 
--- libfterm/ui/ui.c
+++ libfterm/ui/ui.c
@@ -6,7 +6,7 @@
 /*   By: alkuzin <->                                                          */
 /*                                                                            */
 /*   Created: 2023/12/25 21:22:31 by alkuzin                                  */
-/*   Updated: 2023/12/26 09:36:24 by alkuzin                                  */
+/*   Updated: 2023/12/28 12:51:18 by alkuzin                                  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 int primary_color;
 int error_color;
+
+/* print custom colored text */
+static void print_custom_col(const char *string, int color, int style);
+
 
 void set_primary_color(int color)
 {
@@ -23,11 +27,6 @@ void set_primary_color(int color)
 void set_error_color(int color)
 {
     error_color = color;
-}
-
-void print_custom_col(const char *string, int color, int style)
-{
-    printf("\033[%dm\033[%dm%s\033[0m\n", color, style, string);
 }
 
 void print_col(const char *string)
@@ -48,4 +47,19 @@ void print_err_col(const char *string)
 void print_blink_col(const char *string)
 {
     print_custom_col(string, primary_color, STYLE_BLINK);   
+}
+
+void change_col(int color)
+{
+	printf("\033[%dm", color);
+}
+
+void reset_color(void)
+{
+	change_col(STYLE_RESET);
+}
+
+static void print_custom_col(const char *string, int color, int style)
+{
+    printf("\033[%dm\033[%dm%s\033[0m\n", color, style, string);
 }
